330cd67120d34a689bc23e63c7c8d3a3934e5a74
lisenet <tomas@lisenet.com>
2022-12-14 21:47:33 +0000

    Add opsgenie_newrelic_integration_endpoint_url variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,3 +17,9 @@ variable "newrelic_slack_channel_id" {
   description = "Slack channel ID (not name) to send New Relic alerts to."
   type = string
 }
+
+# See https://github.com/lisenet/opsgenie-terraform
+variable "opsgenie_newrelic_integration_endpoint_url" {
+  description = "Endpoint URL for New Relic to post to Opsgenie"
+  type = string
+}
