d2f9476fbcf81427329d16d9a4aeabcde4a887d7
lisenet <tomas@lisenet.com>
2022-12-14 22:09:12 +0000

    Replace newrelic_synthetics_alert_condition with newrelic_synthetics_multilocation_alert_condition

--- terraform/alert_conditions_synthetics.tf
+++ terraform/alert_conditions_synthetics.tf
@@ -1,7 +1,21 @@
-resource "newrelic_synthetics_alert_condition" "main" {
+resource "newrelic_synthetics_multilocation_alert_condition" "main" {
   policy_id = newrelic_alert_policy.alert.id
 
-  name        = "TF Failure www.lisenet.com"
-  monitor_id  = newrelic_synthetics_monitor.main.id
-  enabled     = "true"
+  name        = "TF Failure ${var.website_fqdn_to_monitor}"
+  enabled     = true
+
+  runbook_url                  = "https://${var.website_fqdn_to_monitor}"
+  violation_time_limit_seconds = "86400"
+
+  entities = [
+    newrelic_synthetics_monitor.main.id
+  ]
+
+  critical {
+    threshold = 2
+  }
+
+  warning {
+    threshold = 1
+  }
 }
