f60e3754ef24de8f8811e8241a29af029b16ef67
lisenet <tomas@lisenet.com>
2022-12-14 21:42:49 +0000

    Update synthetics resources to account for new provider version

--- terraform/alert_conditions_synthetics.tf
+++ terraform/alert_conditions_synthetics.tf
@@ -1,14 +1,7 @@
-# Use this resource to create and manage synthetics alert conditions
-data "newrelic_synthetics_monitor" "web_lsn" {
-  name       = "www.lisenet.com_tf"
-  depends_on = [newrelic_synthetics_monitor.web_lsn]
-}
 resource "newrelic_synthetics_alert_condition" "web_lsn_failure" {
   policy_id = newrelic_alert_policy.alert.id
 
   name        = "TF Failure www.lisenet.com"
-  monitor_id  = data.newrelic_synthetics_monitor.web_lsn.id
+  monitor_id  = newrelic_synthetics_monitor.web_lsn.id
   enabled     = "true"
-
-  depends_on  = [newrelic_synthetics_monitor.web_lsn]
 }
